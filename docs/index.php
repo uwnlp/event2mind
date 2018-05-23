430c4dd12a8dbc64f16087cad8838d6d493f2c0a
Maarten Sap <msap@recycle.cs.washington.edu>
2018-05-23 16:20:38 -0700

    removed the php stuff

--- docs/index.php
+++ docs/index.php
@@ -1,29 +1,15 @@
 <!DOCTYPE html>
-<?php
-  header("Access-Control-Allow-Origin: *"); 
-  header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-
-?>
 <head>
   <title>Event2Mind: Commonsense Inference on Events, Intents and Reactions</title>
-  <?php 
-  include("../tools/header.php");
-  include("../tools/nav.php");
-  ?>
-  <script src="js/jquery-3.1.1.min.js"></script>
-  <!--script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script-->
-  <!--script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script-->
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/bootstrap-select.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
   <!--script src="https://d3js.org/d3.v4.min.js" charset="utf-8"></script>
   <script src="https://d3js.org/queue.v1.min.js"></script-->
   <!--script src="http://spin.js.org/spin.min.js"></script-->
-  <script type="text/javascript" src="js/countdown.js"></script>
+
   <script type="text/javascript" src="js/clientside.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML'></script>
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/bootstrap-select.min.css">
-  <link rel="stylesheet" href="css/style.css">
+  
   <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"-->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
