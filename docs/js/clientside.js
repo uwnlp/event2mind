5c9139fb81d43beb73ef0c2bf44eaf016473a45f
Maarten Sap <msap@recycle.cs.washington.edu>
2018-10-05 10:51:45 -0700

    v2 of the website layout

--- docs/js/clientside.js
+++ docs/js/clientside.js
@@ -1,10 +1,12 @@
-var events = {}, eventAnnotations;
+//import {Spinner} from 'spin.js';
+var events = {}, eventAnnotations, event_keys;
 var defaultEvent = "PersonX puts PersonX's trust in PersonY";
 var beautifyRelations = {
   Xintent: "PersonX's intent",
   Xemotion: "PersonX's reaction",
   Otheremotion: "Other people's reaction"
 };
+$(".loadersmall").hide();
 
 jQuery.get( "https://homes.cs.washington.edu/~msap/debug/event2mind/docs/data/event_noAnnots.php", function( data ) {
   var arr;
@@ -12,17 +14,28 @@ jQuery.get( "https://homes.cs.washington.edu/~msap/debug/event2mind/docs/data/ev
     arr = line.split(",",2);
     events[arr[1]] = arr[0];
   });
-  updateSelector();  
+  event_keys = Object.keys(events).sort();
+  updateSelector(true);
 });
 
 
-function updateSelector(){
-  $.each(events,function(k,v){
-    $("#eventSelector").append('<option class="'+v+'"value="'+k+'" data-toggle="'+k+'">'+k+'</option>');
-  });
-  loadEvent(defaultEvent);
+function updateSelector(full){
+  var dataToAdd = full ? event_keys: event_keys.slice(event_keys.indexOf(defaultEvent), event_keys.indexOf(defaultEvent)+5);
+  var dataToAppend = [];
   
+  //console.log("Starting now!");
+  $(".loadersmall").show();
+  $.each(dataToAdd,function(i,k){
+    var v = events[k];
+    //$("#eventSelector").append('<option class="'+v+'"value="'+k+'" data-toggle="'+k+'">'+k+'</option>');
+    dataToAppend.push('<option class="'+v+'"value="'+k+'" data-toggle="'+k+'">'+k+'</option>');
+  });
+  $("#eventSelector").append(dataToAppend);
+  loadEvent(defaultEvent);
+  //console.log("Done!");
   $("#eventSelector").prop("value",defaultEvent).selectpicker("refresh");
+  //console.log("Done2!");
+  $(".loadersmall").hide();
   //$("#eventSelecter").prop("value",defaultEventID).selectpicker("refresh");
 }
 
